4826854d2ecd782bcb77d64f08d42d460079f82c
Couto <couto@musikki.com>
2014-09-10 15:36:49 +0100

    Remove ^M char

--- dnsmasq/fetch_hosts.sh
+++ dnsmasq/fetch_hosts.sh
@@ -23,16 +23,16 @@ host=$(create_tmp_file);
 
 download_file "http://someonewhocares.org/hosts/zero/" "$swch" &
 download_file "http://winhelp2002.mvps.org/hosts.txt" "$mvps" &
-download_file "http://hosts-file.net/download/hosts.txt" "$host" &
+#download_file "http://hosts-file.net/download/hosts.txt" "$host" &
 wait
 
 echo "Formatting lists and creating hosts file";
 grep "127.0.0.1" "$swch" | awk '{ print "0.0.0.0\t"$2 }' >> $TMP_FILE
 grep "0.0.0.0" "$mvps"   | grep -v "# 0.0.0.0" | awk '{ print "0.0.0.0\t"$2 }' >> $TMP_FILE
-grep "127.0.0.1" "$host" | awk '{ print "0.0.0.0\t"$2 }' >> $TMP_FILE
+#grep "127.0.0.1" "$host" | awk '{ print "0.0.0.0\t"$2 }' >> $TMP_FILE
 
 echo "Sorting and removing duplicates"
-tr '[A-Z]' '[a-z]' < "$TMP_FILE" | sort -f | uniq > $ADBLOCK_FILE;
+tr '[A-Z]' '[a-z]' < "$TMP_FILE" | sort -f | uniq -u | sed 's///g' > $ADBLOCK_FILE;
 
 echo "Add local machine as nameserver"
 sudo networksetup -setdnsservers Wi-Fi $(echo "$NAMESERVERS" | tr ' ' '\n' | uniq -u | tr '\n' ' ');
