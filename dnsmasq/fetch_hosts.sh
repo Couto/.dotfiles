3283c4e3805ccf955253ad65ef4767aa0d128e7e
Couto <couto@musikki.com>
2014-09-10 17:08:20 +0100

    Point hosts to 127.0.0.1 to avoid problems

--- dnsmasq/fetch_hosts.sh
+++ dnsmasq/fetch_hosts.sh
@@ -2,14 +2,19 @@
 
 create_tmp_file () {
     local template="$(basename $0)";
-    echo $(mktemp -t $template);
+    echo "$(mktemp -t $template)";
 }
 
 download_file () {
     local output=$2;
     local url=$1
     echo "Downloading $1";
-    curl -s -o $output $url || exit 1
+    curl -s -o "$output" "$url" || exit 1
+}
+
+clean_file () {
+    echo "Cleaning: $1";
+    grep "0.0.0.0\|127.0.0.1" $1 | awk '{ print "127.0.0.1\t"$2 }' >> "$2"
 }
 
 ADBLOCK_FILE="adblock.list";
@@ -17,23 +22,26 @@ TMP_FILE=$(create_tmp_file);
 NAMESERVERS="127.0.0.1 $(networksetup -getdnsservers Wi-Fi)";
 
 
+yoyo=$(create_tmp_file);
 swch=$(create_tmp_file);
 mvps=$(create_tmp_file);
 host=$(create_tmp_file);
 
+download_file "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0" "$yoyo" &
 download_file "http://someonewhocares.org/hosts/zero/" "$swch" &
 download_file "http://winhelp2002.mvps.org/hosts.txt" "$mvps" &
-#download_file "http://hosts-file.net/download/hosts.txt" "$host" &
+download_file "http://hosts-file.net/download/hosts.txt" "$host" &
 wait
 
 echo "Formatting lists and creating hosts file";
-grep "127.0.0.1" "$swch" | awk '{ print "0.0.0.0\t"$2 }' >> $TMP_FILE
-grep "0.0.0.0" "$mvps"   | grep -v "# 0.0.0.0" | awk '{ print "0.0.0.0\t"$2 }' >> $TMP_FILE
-#grep "127.0.0.1" "$host" | awk '{ print "0.0.0.0\t"$2 }' >> $TMP_FILE
+clean_file "$yoyo" "$TMP_FILE";
+clean_file "$swch" "$TMP_FILE";
+clean_file "$mvps" "$TMP_FILE";
+clean_file "$host" "$TMP_FILE";
 
 echo "Sorting and removing duplicates"
 tr '[A-Z]' '[a-z]' < "$TMP_FILE" | sort -f | uniq -u | sed 's///g' > $ADBLOCK_FILE;
 
 echo "Add local machine as nameserver"
-sudo networksetup -setdnsservers Wi-Fi $(echo "$NAMESERVERS" | tr ' ' '\n' | uniq -u | tr '\n' ' ');
+# sudo networksetup -setdnsservers Wi-Fi $(echo "$NAMESERVERS" | tr ' ' '\n' | uniq -u | tr '\n' ' ');
 
