5daa1aa4114781797a33d1dff4cf9ba01abb836b
Couto <couto@musikki.com>
2014-09-10 17:47:54 +0100

    Minor refactoring

--- dnsmasq/fetch_hosts.sh
+++ dnsmasq/fetch_hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-create_tmp_file () {
+tmp_file () {
     local template="$(basename $0)";
     echo "$(mktemp -t $template)";
 }
@@ -13,35 +13,37 @@ download_file () {
 }
 
 clean_file () {
-    echo "Cleaning: $1";
-    grep "0.0.0.0\|127.0.0.1" $1 | awk '{ print "127.0.0.1\t"$2 }' >> "$2"
+    echo "Cleaning: $1"; 
+    grep "0.0.0.0\|127.0.0.1" $1 | awk '{ print "127.0.0.1\t"$2 }' | sed 's///g' >> "$2"
 }
 
-ADBLOCK_FILE="adblock.list";
-TMP_FILE=$(create_tmp_file);
-NAMESERVERS="127.0.0.1 $(networksetup -getdnsservers Wi-Fi)";
+main () {
+    
+    local ADBLOCK_FILE="adblock.list";
+    local TMP_FILE=$(tmp_file);
+    local NAMESERVERS="127.0.0.1 $(networksetup -getdnsservers Wi-Fi)";
 
+    local yoyo=$(tmp_file);
+    local swch=$(tmp_file);
+    local mvps=$(tmp_file);
+    local host=$(tmp_file);
 
-yoyo=$(create_tmp_file);
-swch=$(create_tmp_file);
-mvps=$(create_tmp_file);
-host=$(create_tmp_file);
+    download_file "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0" "$yoyo" &
+    download_file "http://someonewhocares.org/hosts/zero/" "$swch" &
+    download_file "http://winhelp2002.mvps.org/hosts.txt" "$mvps" &
+    download_file "http://hosts-file.net/download/hosts.txt" "$host" &
+    wait
 
-download_file "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0" "$yoyo" &
-download_file "http://someonewhocares.org/hosts/zero/" "$swch" &
-download_file "http://winhelp2002.mvps.org/hosts.txt" "$mvps" &
-download_file "http://hosts-file.net/download/hosts.txt" "$host" &
-wait
+    clean_file "$yoyo" "$TMP_FILE";
+    clean_file "$swch" "$TMP_FILE";
+    clean_file "$mvps" "$TMP_FILE";
+    clean_file "$host" "$TMP_FILE";
 
-echo "Formatting lists and creating hosts file";
-clean_file "$yoyo" "$TMP_FILE";
-clean_file "$swch" "$TMP_FILE";
-clean_file "$mvps" "$TMP_FILE";
-clean_file "$host" "$TMP_FILE";
+    echo "Sorting and removing duplicates"
+    tr '[A-Z]' '[a-z]' < "$TMP_FILE" | sort -f | uniq -u > $ADBLOCK_FILE;
 
-echo "Sorting and removing duplicates"
-tr '[A-Z]' '[a-z]' < "$TMP_FILE" | sort -f | uniq -u | sed 's///g' > $ADBLOCK_FILE;
+    echo "Add local machine as nameserver"
+    sudo networksetup -setdnsservers Wi-Fi $(echo "$NAMESERVERS" | tr ' ' '\n' | uniq -u | tr '\n' ' ');
+}
 
-echo "Add local machine as nameserver"
-# sudo networksetup -setdnsservers Wi-Fi $(echo "$NAMESERVERS" | tr ' ' '\n' | uniq -u | tr '\n' ' ');
-
+main;
