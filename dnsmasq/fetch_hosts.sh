5299a37f812d8f1f3c10670733bb7b045c320b31
Luis Couto <couto@15minuteslate.net>
2014-09-11 22:12:07 +0100

    Refine the hosts list

--- dnsmasq/fetch_hosts.sh
+++ dnsmasq/fetch_hosts.sh
@@ -14,7 +14,7 @@ download_file () {
 
 clean_file () {
     echo "Cleaning: $1"; 
-    grep "0.0.0.0\|127.0.0.1" $1 | awk '{ print "127.0.0.1\t"$2 }' | sed 's///g' >> "$2"
+    grep "0.0.0.0\|127.0.0.1" $1 | awk '{ print "0.0.0.0\t"$2 }' | sed 's///g' >> "$2"
 }
 
 main () {
@@ -23,20 +23,26 @@ main () {
     local TMP_FILE=$(tmp_file);
 
     local yoyo=$(tmp_file);
-    local swch=$(tmp_file);
+    local adaway=$(tmp_file);
     local mvps=$(tmp_file);
-    local host=$(tmp_file);
+    # local host=$(tmp_file);
+    local hostads=$(tmp_file);
+    local malware=$(tmp_file);
 
     download_file "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0" "$yoyo" &
-    download_file "http://someonewhocares.org/hosts/zero/" "$swch" &
+    download_file "https://adaway.org/hosts.txt" "$adaway" &
     download_file "http://winhelp2002.mvps.org/hosts.txt" "$mvps" &
-    download_file "http://hosts-file.net/download/hosts.txt" "$host" &
+    # download_file "http://hosts-file.net/download/hosts.txt" "$host" &
+    download_file "http://hosts-file.net/ad_servers.txt" "$hostads" &
+    download_file "http://www.malwaredomainlist.com/hostslist/hosts.txt" $malware &
     wait
-
+    
     clean_file "$yoyo" "$TMP_FILE";
-    clean_file "$swch" "$TMP_FILE";
+    clean_file "$adaway" "$TMP_FILE";
     clean_file "$mvps" "$TMP_FILE";
-    clean_file "$host" "$TMP_FILE";
+    # clean_file "$host" "$TMP_FILE";
+    clean_file "$hostads" "$TMP_FILE";
+    clean_file "$malware" "$TMP_FILE";
 
     echo "Sorting and removing duplicates"
     tr '[A-Z]' '[a-z]' < "$TMP_FILE" | sort -f | uniq > $ADBLOCK_FILE;
