a743d500a23888a9caba46cbfd86796d2acbdb57
Couto <couto@15minuteslate.net>
2014-09-12 22:39:15 +0100

    Aliases. Keybindings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,10 +3,14 @@
 # Alias
 alias gi='gist -c -p -P -s -t js'
 alias poopin='open /System/Library/Frameworks/ScreenSaver.framework/Versions/Current/Resources/ScreenSaverEngine.app/'
-alias wanip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias flush="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-
 alias fuck='sudo'
+
+# Network
+alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias flush='dscacheutil -flushcache && sudo killall -HUP mDNSResponder'
+alias whoiss='whois -h whois-servers.net'
+alias http-serve='python -m SimpleHTTPServer'
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
 alias l='ls -lahG'
 
@@ -14,6 +18,7 @@ alias l='ls -lahG'
 alias cp='cp -i'
 alias rm='rm -i'
 alias mv='mv -i'
+alias ln='ln -i'
 
 # because typing 'cd' is A LOT of work!!
 alias ..='cd ../'
--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -3,3 +3,32 @@
 # History Substring Search key bindings
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+
+# Search backward incrementally for a specified string
+bindkey '^r' history-incremental-search-backward # ctrl+r
+
+# Cursor movements
+bindkey '^a' beginning-of-line # ctrl+a
+bindkey '^e' end-of-line # ctrl+e
+
+# [Backspace] - delete backward
+bindkey '^?' backward-delete-char
+
+# [Delete] - delete forward
+if [[ "${terminfo[kdch1]}" != "" ]]; then
+    bindkey "${terminfo[kdch1]}" delete-char 
+else
+    bindkey "^[[3~" delete-char
+    bindkey "^[3;5~" delete-char
+    bindkey "\e[3~" delete-char
+fi
+
+# [Home] - Go to beginning of line
+if [[ "${terminfo[khome]}" != "" ]]; then
+    bindkey "${terminfo[khome]}" beginning-of-line
+fi
+
+# [End] - Go to end of line
+if [[ "${terminfo[kend]}" != "" ]]; then
+    bindkey "${terminfo[kend]}"  end-of-line
+fi
