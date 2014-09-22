0ab57ecd38767a630aca15d3b6c053dc8df35ae2
Couto <couto@15minuteslate.net>
2014-09-22 10:45:02 +0100

    Update dnsmasq scripts. Update iTerm settings.

--- dnsmasq/restart_dnsmasq.sh
+++ dnsmasq/restart_dnsmasq.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 if ! { [ "$TERM" = "screen" ] || [ -n "$TMUX" ]; } then
-    sudo launchctl stop homebrew.mxcl.dnsmasq;
-    sudo launchctl start homebrew.mxcl.dnsmasq;
+    echo "Stopping homebrew.mxcl.dnsmasq";
+    sudo launchctl unload /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+
+    echo "Launching homebrew.mxcl.dnsmasq"
+    sudo cp -fv /usr/local/opt/dnsmasq/*.plist /Library/LaunchDaemons
+    sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+
+    echo "Flushing DNS cache"
+    dscacheutil -flushcache
+    sudo killall -HUP mDNSResponder
 else
     echo "ERROR: launchctl doesn't work inside tmux or screen sessions."
 fi
