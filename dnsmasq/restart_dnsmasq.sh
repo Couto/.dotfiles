63b29bbf0ee4a488721cb34c71006a99a0b21094
Couto <couto@15minuteslate.net>
2014-10-13 10:17:19 +0100

    Confirm with user if they can reload dnsmasq inside tmux.

--- dnsmasq/restart_dnsmasq.sh
+++ dnsmasq/restart_dnsmasq.sh
@@ -1,16 +1,42 @@
-#!/bin/bash
+#!/user/bin/env bash
 
-if ! { [ "$TERM" = "screen" ] || [ -n "$TMUX" ]; } then
-    echo "Stopping homebrew.mxcl.dnsmasq";
+set -euo pipefail
+IFS=$'\n\t'
+
+prompt () {
+    echo "$@";
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) return 0; exit;;
+            No ) return 1; exit;;
+        esac
+    done
+}
+
+restart_dnsmasq () {
     sudo launchctl unload /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
 
-    echo "Launching homebrew.mxcl.dnsmasq"
     sudo cp -fv /usr/local/opt/dnsmasq/*.plist /Library/LaunchDaemons
     sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
 
-    echo "Flushing DNS cache"
     dscacheutil -flushcache
     sudo killall -HUP mDNSResponder
+}
+
+
+if ! { [ "$TERM" = "screen" ] || [ -n "$TMUX" ]; } then
+
+    restart_dnsmasq;
+
 else
-    echo "ERROR: launchctl doesn't work inside tmux or screen sessions."
+ 
+    prompt "
+
+    To restart dnsmasq, launchctl must be used, however launchctl 
+    doesn't work inside tmux, unless you're running `reattach-to-user-namespace`
+    
+    Do you still want to attempt to restart dnsmasq?
+    
+    " && restart_dnsmasq;   
+
 fi
