efc6af3aac583b85156e42580bff911eac18d6fb
Couto <couto@15minuteslate.net>
2014-11-04 22:32:06 +0000

    Add hosts files.

--- dnsmasq/fetch_hosts.sh
+++ dnsmasq/fetch_hosts.sh
@@ -72,6 +72,8 @@ main () {
         "https://adaway.org/hosts.txt"
         "http://hosts-file.net/ad_servers.txt"
         "http://www.malwaredomainlist.com/hostslist/hosts.txt"
+        "http://someonewhocares.org/hosts/zero/hosts"
+        "http://winhelp2002.mvps.org/hosts.txt"
     );
 
     for host in "${HOSTS_FILES[@]}";
